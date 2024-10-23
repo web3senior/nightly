42d4d1b9c88ecddaec0ec79390675fd4718e8a8e
web3senior <atenyun@gmail.com>
2024-10-24 01:22:33 +0300

    Add category name

--- src/src/routes/project.jsx
+++ src/src/routes/project.jsx
@@ -31,7 +31,7 @@ export default function Owned({ title }) {
 
   const handleFilter = (categoryItem) => {
     const newData = backupData.project.filter((item) => categoryItem.id === item.category_id)
- setData({ project: newData })
+    setData({ project: newData })
   }
 
   useEffect(() => {
@@ -57,16 +57,16 @@ export default function Owned({ title }) {
 
         {category && category.length > 0 && (
           <div className={`${styles['category']} d-flex flex-row align-items-start justify-content-start`}>
-           <ul>
-           {category.map((item, i) => {
-              return (
-                <li key={i} className={`d-f-c`} onClick={() => handleFilter(item)}>
-                  <div dangerouslySetInnerHTML={{ __html: item.icon }} />
-                  <b>{item.name}</b>
-                </li>
-              )
-            })}
-           </ul>
+            <ul>
+              {category.map((item, i) => {
+                return (
+                  <li key={i} className={`d-f-c`} onClick={() => handleFilter(item)}>
+                    <div dangerouslySetInnerHTML={{ __html: item.icon }} />
+                    <b>{item.name}</b>
+                  </li>
+                )
+              })}
+            </ul>
           </div>
         )}
 
@@ -76,13 +76,19 @@ export default function Owned({ title }) {
               return (
                 <div key={i} className={`card w-100 mt-10`}>
                   <div className={`card__body d-flex flex-row align-items-center justify-content-between`} style={{ columnGap: `1rem` }}>
-                   
-                   <div className={`d-flex flex-column`}>
-                   <b>{item.title}</b>
-                    <small className={``} style={{ color: `var(--black-100)` }}>
-                      {item.lead}
-                    </small>
-                   </div>
+                    <ul className={`d-flex flex-column`}>
+                      <li>
+                        <b>{item.title}</b>
+                      </li>
+                      <li className={``} style={{ color: `var(--black-100)` }}>
+                        {item.lead}
+                      </li>
+                      {category && category.length > 0 && (
+                        <li>
+                          <span className={`badge badge-pill badge-warning`}>{category.filter((filteredItem, i) => filteredItem.id === item.category_id)[0].name}</span>
+                        </li>
+                      )}
+                    </ul>
 
                     <Link to={`/project/${item.id}`}>View</Link>
                   </div>
